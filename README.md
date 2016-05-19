e7184de120484f2aa9b6ae93c5e8ed60d06fab43
Nick Irvine <nfirvine@nfirvine.com>
2016-05-19 13:25:20 -0700

    sed s/yubikey/usb/g

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# osx-lock-on-remove-yubikey
-Lock the screen on OS X when my yubikey is removed
+# osx-lock-on-remove-usb
+Lock the screen on OS X when some usb device is removed
 
 # Installation
 
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp osx-lock-on-remove-yubikey.sh /usr/local/bin
-chmod +x /usr/local/bin/osx-lock-on-remove-yubikey.sh
-cp osx-lock-on-remove-yubikey.plist ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/com.nfirvine.osx-lock-on-remove-yubikey.plist
+cp osx-lock-on-remove-usb.sh /usr/local/bin
+chmod +x /usr/local/bin/osx-lock-on-remove-usb.sh
+cp osx-lock-on-remove-usb.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/com.nfirvine.osx-lock-on-remove-usb.plist
