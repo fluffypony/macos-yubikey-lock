139244c64b98945a9e00f9debe8360e89f8cf86b
Nick Irvine <nfirvine@nfirvine.com>
2016-05-19 15:36:22 -0700

    Use screen saver instead of login window (keeps wifi up)

--- osx-lock-on-remove-usb.sh
+++ osx-lock-on-remove-usb.sh
@@ -7,7 +7,7 @@ prev_state="unknown"
 state="unknown"
 
 function lock {
-  /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+  open -a /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app
 }
 
 while true; do
