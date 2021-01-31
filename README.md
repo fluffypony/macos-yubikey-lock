7b468bd91c203016f552b8d0cbe47bc69e5af5e0
Riccardo Spagni <ric@spagni.net>
2021-01-31 18:53:11 +0200

    remove all the vendor stuff, rename files, update readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # osx-lock-on-remove-usb
-Lock the screen on OS X when some usb device is removed
+
+Lock the screen on OS X when your YubiKey is removed
 
 # Installation
 
-	./install.sh <vendor id> <product id>
+	./install.sh
+
+# Advanced
+
+This polls ```ioreg -p IOUSB -l -w 0```, which is MUCH MUCH faster than polling system_profiler. If you use something other than a YubiKey, just find something unique (eg. the ID that comes after the ```class AppleUSBDevice``` for it) and replace the egrep in ```osx-lock-on-remove-usb.sh```.
--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh -eux
-if [ $# -lt 2 ]; then
-	echo 'usage: install.sh <vendor_id> <product_id>'
-fi
-vendor_id=$1
-product_id=$2
-out=com.nfirvine.osx-lock-on-remove-usb-${vendor_id}-${product_id}.plist
+
 cp osx-lock-on-remove-usb.sh /usr/local/bin
 chmod +x /usr/local/bin/osx-lock-on-remove-usb.sh
-sed -e "s/{{vendor_id}}/$vendor_id/g" -e "s/{{product_id}}/$product_id/g" < com.nfirvine.osx-lock-on-remove-usb.plist.template > $out
-cp $out ~/Library/LaunchAgents
-launchctl load ~/Library/LaunchAgents/$out
+cp com.fluffypony.osx-lock-on-remove-usb.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
