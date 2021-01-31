53a33ff0c4c506e251307251f4efe63bd4c63940
Riccardo Spagni <ric@spagni.net>
2021-01-31 18:49:01 +0200

    switch to using ioreg (instant) and just search for YubiKey

--- osx-lock-on-remove-usb.sh
+++ osx-lock-on-remove-usb.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -u
 
-vendor_id=$1
-prod_id=$2
-
 prev_state="unknown"
 state="unknown"
 
@@ -12,7 +9,7 @@ function lock {
 
 while true; do
   prev_state=$state
-  if $(system_profiler SPUSBDataType | tr '\n' '\t' | egrep "Product ID: 0x$prod_id\\s+Vendor ID: 0x$vendor_id" > /dev/null 2>&1); then
+  if $(ioreg -p IOUSB -l -w 0 | egrep "YubiKey" > /dev/null 2>&1); then
     state="attached"
   else
     state="detached"
