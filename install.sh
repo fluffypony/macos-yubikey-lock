96b512d886f8e170e9facb750ba1d0415d3b8b5c
Matt Lucas <mattlucas719@gmail.com>
2021-08-23 10:45:11 -0500

    adding support for re running install.sh
    we remove existing files and re-populate with new incase edits were made

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -eux
 
+rm -f /usr/local/bin/osx-lock-on-remove-usb.sh
 cp osx-lock-on-remove-usb.sh /usr/local/bin
 chmod +x /usr/local/bin/osx-lock-on-remove-usb.sh
+rm -f ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
 cp com.fluffypony.osx-lock-on-remove-usb.plist ~/Library/LaunchAgents
+launchctl unload ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
 launchctl load ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
