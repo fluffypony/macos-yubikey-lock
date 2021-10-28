d904c409f83dce110d64c9ad5e06274a102d8e94
Riccardo Spagni <ric@spagni.net>
2021-10-28 13:04:37 -0500

    update to work with M1 homebrew

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 
-cp osx-lock-on-remove-usb.sh /usr/local/bin
-chmod +x /usr/local/bin/osx-lock-on-remove-usb.sh
+cp osx-lock-on-remove-usb.sh /opt/homebrew/bin
+chmod +x /opt/homebrew/bin/osx-lock-on-remove-usb.sh
 cp com.fluffypony.osx-lock-on-remove-usb.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/com.fluffypony.osx-lock-on-remove-usb.plist
